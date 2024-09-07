5ba956c6552787250d59a47f3cbf5e07b1943c99
Galmiche Lucas <galmicheluas59@gmail.com>
2024-09-07 20:06:14 +0200

    setFilter started

--- FrontEnd/JS/app.js
+++ FrontEnd/JS/app.js
@@ -25,3 +25,30 @@ figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
  
 document.querySelector(".gallery").append(figure);
 }
+
+async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
+    try{
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error('Response status: ${response.status}');
+        }
+
+        const json = await response.json();
+        console.log(json);
+        for(let i = 0; i < json.length; i++) {
+            setFilter(json[i]); 
+        }
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+getCategories();
+
+
+function setFilter(data) {
+    const div = document.createElement("div")
+div.innerHTML = `${data.name}`;
+ 
+document.querySelector(".div-container").append(div);
+}
